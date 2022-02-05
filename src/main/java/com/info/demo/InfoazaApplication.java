40d2c128f8a4ee2e2e74fa38ee0021b53e0130d2
Harmeet <harmeetwalia552@gmail.com>
2022-02-05 14:47:03 +0530

    hello

---
+++ src/main/java/com/info/demo/InfoazaApplication.java
@@ -0,0 +1,13 @@
+package com.info.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoazaApplication.class, args);
+	}
+
+}
